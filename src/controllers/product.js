f6813605ee73a66c38022e306b8a5949f1dd6873
Nguyễn Văn Phong <122197487+Phong25102003@users.noreply.github.com>
2023-11-28 18:48:43 +0700

    first commit

---
+++ src/controllers/product.js
@@ -0,0 +1,129 @@
+import Product from "../models/Product";
+import { productValidate } from "../validations/productValid";
+
+export const getAllProduct = async (req, res) => {
+    try {
+        const data = await Product.find();
+        if (!data || !data.length) {
+            return res.status(404).json({
+                message: "Khong co san pham nao!",
+                data: [],
+            });
+        }
+        return res.status(200).json({
+            message: "Lay danh sach san pham thanh cong!",
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            name: error.name || "Error",
+            message: error.message || "Server error!",
+        });
+    }
+};
+
+export const getDetailProduct = async (req, res) => {
+    try {
+        const data = await Product.findById(req.params.id);
+        if (!data) {
+            return res.status(404).json({
+                message: "Not found!",
+                data: [],
+            });
+        }
+        return res.status(200).json({
+            message: "Successfully!",
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            name: error.name || "Error",
+            message: error.message || "Server error!",
+        });
+    }
+};
+
+export const removeProduct = async (req, res) => {
+    try {
+        const data = await Product.findByIdAndDelete(req.params.id);
+        if (!data) {
+            return res.status(404).json({
+                message: "Remove failed!",
+            });
+        }
+        return res.status(200).json({
+            message: "Successfully!",
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            name: error.name || "Error",
+            message: error.message || "Server error!",
+        });
+    }
+};
+
+export const createProduct = async (req, res) => {
+    try {
+        const body = req.body;
+        const { error } = productValidate.validate(body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((item) => item.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+        if (!body) {
+            return res.status(400).json({
+                message: "Data is required!",
+            });
+        }
+
+        const data = await Product.create(body);
+
+        if (!data) {
+            return res.status(400).json({
+                message: "Create failed!",
+            });
+        }
+        return res.status(200).json({
+            message: "Successfully!",
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            name: error.name || "Error",
+            message: error.message || "Server error!",
+        });
+    }
+};
+
+export const updateProduct = async (req, res) => {
+    try {
+        const body = req.body;
+        const { error } = productValidate.validate(body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((item) => item.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+        const data = await Product.findByIdAndUpdate(req.params.id, body, {
+            new: true,
+        });
+        if (!data) {
+            return res.status(400).json({
+                message: "Update failed!",
+            });
+        }
+        return res.status(200).json({
+            message: "Successfully!",
+            data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            name: error.name || "Error",
+            message: error.message || "Server error!",
+        });
+    }
+};
